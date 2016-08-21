8d6889a28254db007646bc4c5bd0aa2acd6bc66e
peilei520 <3141219541@qq.com>
2016-08-21 11:15:46 +0800

    first project

---
+++ src/main/java/project/Login.java
@@ -0,0 +1,11 @@
+package project;
+
+import com.peilei.framework.BaseCase;
+
+public class Login extends BaseCase{
+	
+	public void aa(){
+		System.out.println("open browser");
+		driver.get("https://www.baidu.com");
+	}
+}
